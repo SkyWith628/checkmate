b12e2767c335eab7a8f823cb91cabed63c4e2494
june <jmjmin9232@gmail.com>
2026-06-09 15:47:49 +0900

    feat/main

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDsAGQKW220ixuanCMzcEskiEJTkYhJDYI",
+  authDomain: "check-mate-14014.firebaseapp.com",
+  projectId: "check-mate-14014",
+  storageBucket: "check-mate-14014.firebasestorage.app",
+  messagingSenderId: "427742121784",
+  appId: "1:427742121784:web:65bb1118af9cb00e34bbcd"
+};
